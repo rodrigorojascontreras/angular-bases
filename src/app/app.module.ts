14c992e9ec8eeacbf4f2273f6b08b01ca385096a
Rodrigo Rojas <rodrigorojascontreras@gmail.com>
2023-12-15 04:42:30 -0300

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './heroes/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
@@ -10,6 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
