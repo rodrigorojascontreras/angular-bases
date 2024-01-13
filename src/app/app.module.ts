90b4cb670469bc6b372d98322156ae3a3b5d0d0c
Rodrigo Rojas <rodrigorojascontreras@gmail.com>
2024-01-13 06:29:19 -0300

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './heroes/list/list.component';
-import { HeroesModule } from './heroes/heroes.module';
 
 
 @NgModule({
@@ -17,6 +17,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
     AppRoutingModule
   ],
   providers: [],
